cd6ce780344a51ff1c3dc3492abd33be56e79311
Devabhi03 <devabhi2603@gmail.com>
2024-01-08 14:49:07 +0530

    pattern4

---
+++ pattern4.cpp
@@ -0,0 +1,29 @@
+/*
+now we have to print pattern like this -
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+void print1(int n){
+for(int i =1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout << i << " ";
+    }
+    cout << endl;
+}
+}
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+}
