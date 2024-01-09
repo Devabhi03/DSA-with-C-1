54c81741033f18d6f33843c136f43ffb67ecb468
Devabhi03 <devabhi2603@gmail.com>
2024-01-09 22:53:37 +0530

    pattern7

--- pattern7.cpp
+++ pattern7.cpp
@@ -1,11 +1,12 @@
 /*
 now we have to print pattern like this -
 
-1 2 3 4 5 
-1 2 3 4 
-1 2 3 
-1 2 
-1
+     *
+    ***
+   *****
+  *******
+ *********
+***********
 
 */
 
@@ -15,9 +16,20 @@ now we have to print pattern like this -
 using namespace std;
 
 void print1(int n){
-for(int i =1;i<=n;i++){
-    for(int j=1;j<=n-i+1;j++){
-        cout << j <<  " ";
+for(int i =0;i<=n;i++){
+    //space
+    for(int j=0;j<n-i-1;j++){
+        cout <<  " ";
+    }
+
+    //star
+      for(int j=0;j<2*i+1;j++){
+        cout <<  "*";
+    }
+
+    //space
+      for(int j=0;j<n-i-1;j++){
+        cout <<  " ";
     }
     cout << endl;
 }
