1b0bdc4c026020aeb0f9d31b9a282c54bfb83cc3
Devabhi03 <devabhi2603@gmail.com>
2024-03-11 11:38:59 +0530

    1140

---
+++ c++ from 09 march 2024/lecture_1_agefrom25to100.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    // 25 min video tag
+    return 0;
+}
---
+++ c++ from 09 march 2024/lecture_1basiconeshot.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+// #include <bits/stdc++.h> is used to include all ths libraries in c++
+
+int main() {
+    std::cout << "Hello, World!"<<std::endl<<"this is abhinandan"<<std::endl;
+    return 0;
+// this is how we write basic structure of c++ code
+
+return 0;
+int x,y;
+cin>>x>>y;
+cout<<"value of x"<<x<<"value of y"<<y;
+return 0;
+
+/*types of data type we can use in c++ for integre numbers
+1)int
+2)long
+3)longlong  
+
+for decimal values we can use
+1)float
+2)double
+
+for string we can use 
+1)string- string(only for storing one word till space)
+2)getline+string -use get line with  string to store entire line
+3)char- to store one alphabet we will use this*/
+
+string s1,s2;
+cin>>s1;
+cout<<s1;
+getline(cin,s2);
+cout<<s2;
+// enter sentence like hello myself raj to see the working
+
+return 0;
+
+
+}
