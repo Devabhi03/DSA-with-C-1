0de34e13ef3fffbaa1890cdc7fa583d921636083
Devabhi03 <devabhi2603@gmail.com>
2024-03-13 12:20:15 +0530

    3107

--- c++ from 09 march 2024/lecture_1_agefrom25to100.cpp
+++ c++ from 09 march 2024/lecture_1_agefrom25to100.cpp
@@ -1,7 +1,29 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-int main(){
-    // 25 min video tag
+int main()
+{
+    int marks;;
+    cin>>marks;
+    if(marks>=80&&marks<100){
+        cout<<"A"<<endl;
+    }
+    if(marks>=60&&marks<80){
+        cout<<"B"<<endl;
+    }
+    if(marks>=50&&marks<60){
+        cout<<"C"<<endl;
+    }
+    if(marks>=45&&marks<50){
+        cout<<"D"<<endl;
+    }
+    if(marks>=25&&marks<45){
+        cout<<"E"<<endl;
+    }
+    if(marks<25){
+        cout<<"F"<<endl;
+    }
+
+
     return 0;
 }
